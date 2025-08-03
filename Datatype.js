bb07ef58a9c6c278e9ada8fcf06da0b04b5f5008
Kaushik Prasad <kaushikprasad1659@gmail.com>
2025-08-04 04:36:19 +0530

    notes

--- Datatype.js
+++ Datatype.js
@@ -1,3 +1,6 @@
 "use Strict";  //treat all js code as newer version
 
-alert(6+6);
+alert(6+6);  //If we are using node js then its giving an error  , If not using node js then it will rungit init perfectly
+console.log(alert(6+6));  //This will print undefined because alert function does not return anything
+
+
